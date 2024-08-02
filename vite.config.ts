2017871d5ab615121ea59fd8e04a13bb6cbe190e
lstpxl <115099687+lstpxl@users.noreply.github.com>
2024-08-02 11:56:55 +0200

    Try loadEnv

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,26 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
   // @ts-expect-error Property 'env' does not exist on type 'ImportMeta'.ts
-  base: import.meta.env.BASE_URL,
-});
+  console.log("import.meta.env.BASE_URL=", import.meta.env.BASE_URL);
+  // @ts-expect-error Property 'env' does not exist on type 'ImportMeta'.ts
+  console.log("import.meta.env.VITE_BASE_URL=", import.meta.env.VITE_BASE_URL);
+  // @ts-expect-error Property 'env' does not exist on type 'ImportMeta'.ts
+  console.log("import.meta.env.VITE_DEP_URL=", import.meta.env.VITE_DEP_URL);
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    // @ts-expect-error Property 'env' does not exist on type 'ImportMeta'.ts
+    base: import.meta.env.BASE_URL,
+  });
+};
